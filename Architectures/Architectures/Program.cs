33b354349969e76dade6d3712356de391cf4a006
zhang.w.w <zhang.w.w@foxmail.com>
2018-05-21 22:21:44 +0800

    Base code.

---
+++ Architectures/Architectures/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architectures
+{
+    public class Program
+    {
+        static Int32 Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
