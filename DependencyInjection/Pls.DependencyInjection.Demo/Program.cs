eae701e88bd36fd7992d9f4386cf16fc34ecfeca
zhang weiwei <zhangweiweish@users.noreply.github.com>
2018-06-25 15:40:45 +0800

    Dependency Injection

---
+++ DependencyInjection/Pls.DependencyInjection.Demo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonServiceLocator;
+using Unity;
+
+namespace Pls.DependencyInjection.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnityContainer container = new UnityContainer();
+            RegisterInstance(container);
+
+            ServiceLocator.Current.GetInstance<Shell>();
+
+            Console.ReadKey();
+        }
+        
+        private static void RegisterInstance(IUnityContainer container)
+        {
+            container.RegisterType<IClass,ClassA>();
+            IClass cls1 = container.Resolve<IClass>();
+            Console.WriteLine(cls1.GetHashCode());
+            cls1 = container.Resolve<IClass>();
+            Console.WriteLine(cls1.GetHashCode());
+
+            IClass cls2 = new ClassA();
+            cls2.Name = "Cls2";
+            container.RegisterInstance<IClass>(cls2);
+
+            IClass cls3 = new ClassA();
+            cls3.Name = "Cls3";
+            container.RegisterInstance< IClass>("Cls3", cls3);
+
+            Console.WriteLine(container.Resolve<IClass>().Name);
+            Console.WriteLine(container.Resolve<IClass>("Cls3").Name);
+        }
+    }
+
+    public interface IClass
+    {
+        string Name { get; set; }
+    }
+
+    public class ClassA : IClass
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ DependencyInjection/Pls.MEF.Demo/Books.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.MEF.Demo
+{
+    [Export("MusicBook", typeof(IBookService))]
+    public class MusicBook : IBookService
+    {
+        //导出私有属性
+        [Export(typeof(string))]
+        private string _privateBookName = "Private Music BookName";
+
+        //导出公有属性
+        [Export(typeof(string))]
+        public string _publicBookName = "Public Music BookName";
+
+
+        public string BookName { get; set; }
+
+        [Export(typeof(Func<string>))]
+        public string GetBookName()
+        {
+            return "MusicBook";
+        }
+
+        //导出私有方法
+        [Export(typeof(Func<int, string>))]
+        private string GetBookPrice(int price)
+        {
+            return "$" + price;
+        }
+    }
+
+    [Export("MathBook", typeof(IBookService))]
+    public class MathBook : IBookService
+    {
+        public string BookName { get; set; }
+
+        public string GetBookName()
+        {
+            return "MathBook";
+        }
+    }
+
+    [Export("HistoryBook", typeof(IBookService))]
+    public class HistoryBook : IBookService
+    {
+        public string BookName { get; set; }
+
+        public string GetBookName()
+        {
+            return "HistoryBook";
+        }
+    }
+
+
+}
