1543cc228dc856340e2610094287465cfe3ec120
Zhang, Wei-wei <wei-wei.zhang@perkinelmer.com>
2018-06-26 15:42:16 +0800

    Flyweight

---
+++ Architectures/FlyWeight/Pls.FlyWeight.Implementation/Experiment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.FlyWeight.Implementation
+{
+    public class Experiment
+    {
+        public DateTime CreateTime { get; set; }
+
+        private SpectrumData _spectrum = null;
+
+        public SpectrumData Spectrum
+        {
+            get
+            {
+                return _spectrum;
+            }
+            set
+            {
+                _spectrum = SpectrumDataManager.Instance.StoreData(value);
+            }
+        }
+    }
+}
---
+++ Architectures/FlyWeight/Pls.FlyWeight.Implementation/SpectrumDataManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.FlyWeight.Implementation
+{
+    public class SpectrumDataManager
+    {
+        private static SpectrumDataManager _manager = null;
+
+        public static SpectrumDataManager Instance
+        {
+            get
+            {
+                if (_manager == null)
+                {
+                    _manager = new SpectrumDataManager();
+                    _manager.FlyWeight = true;
+                }
+                return _manager;
+            }
+        }
+
+        private List<SpectrumData> _datas = new List<SpectrumData>();
+
+        public SpectrumData StoreData(SpectrumData data)
+        {
+            if (FlyWeight)
+            {
+                foreach (SpectrumData spectrum in _datas)
+                {
+                    if (spectrum.Equals(data))
+                        return spectrum;
+                }
+            }
+
+            if(data != null)
+                _datas.Add(data);
+            return data;
+        }
+
+        public bool FlyWeight { get; set; }
+    }
+}
