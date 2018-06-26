c9d657b877e0745b6bd18f14c29ae2bca769ffd9
zhang.w.w <wei-wei.zhang@perkinelmer.com>
2018-06-26 22:31:25 +0800

    TemplateMethod

---
+++ Architectures/TemplateMethod/Pls.TemplateMethod.Implementation/DbAccessUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.TemplateMethod.Implementation
+{
+    public class DbAccessUser : DbAccess
+    {
+        protected override void Select()
+        {
+            Console.WriteLine("DbAccessUser::Fill Db Adapter");
+        }
+
+        protected override void Display()
+        {
+            Console.WriteLine("DbAccessUser::Display");
+        }
+    }
+
+    public class DbAccessGroup : DbAccess
+    {
+        protected override void Select()
+        {
+            Console.WriteLine("DbAccessGroup::Fill Db Adapter");
+        }
+
+        protected override void Display()
+        {
+            Console.WriteLine("DbAccessGroup::Display");
+        }
+    }
+}
