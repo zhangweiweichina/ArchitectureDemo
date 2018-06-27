c684a113106f2e8ca69a27788dc9df44477f335e
zhang weiwei <zhangweiweish@users.noreply.github.com>
2018-06-27 23:26:14 +0800

    Command

---
+++ Architectures/Command/Pls.Command.Contract/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.Command.Contract
+{
+    public interface ICommand
+    {
+        void Execute(IList<string> arrList);
+
+        void Undo(IList<string> arrList);
+    }
+}
