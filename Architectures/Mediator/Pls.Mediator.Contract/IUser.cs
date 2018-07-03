eaba430678c3322b8d544fd6a2be9cdb73f6662d
zhang weiwei <zhangweiweish@users.noreply.github.com>
2018-07-03 23:40:59 +0800

    Mediator

---
+++ Architectures/Mediator/Pls.Mediator.Contract/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.Mediator.Contract
+{
+    public interface IUser
+    {
+        IMediator Mediator { get; set; }
+
+        string Name { get; set; }
+
+        void Receive(string from, string message);
+
+        void SendMessage(string to, string message);
+    }
+}
---
+++ Architectures/Mediator/Pls.Mediator.Implementation/Mediator.cs
@@ -0,0 +1,30 @@
+﻿using Pls.Mediator.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.Mediator.Implementation
+{
+    public class Mediator : IMediator
+    {
+        protected Dictionary<string, IUser> _users = new Dictionary<string, IUser>();
+        public void Register(IUser user)
+        {
+            if(!_users.ContainsKey(user.Name))
+            {
+                _users.Add(user.Name, user);
+                user.Mediator = this;
+            }
+        }
+
+        public void SendTo(string from, string to, string strMessage)
+        {
+            if(_users.ContainsKey(to))
+            {
+                _users[to].Receive(from, strMessage);
+            }
+        }
+    }
+}
