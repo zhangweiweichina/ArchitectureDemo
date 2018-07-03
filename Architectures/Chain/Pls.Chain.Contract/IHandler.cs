5aa83ccf464df88eb8364a9fa857cf53a337d7d2
zhang weiwei <zhangweiweish@users.noreply.github.com>
2018-07-03 23:09:18 +0800

    Chain

---
+++ Architectures/Chain/Pls.Chain.Contract/IHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.Chain.Contract
+{
+    public interface IHandler
+    {
+        IHandler Next { get; set; }
+
+        void Handle(double dbvalue);
+    }
+}
---
+++ Architectures/Chain/Pls.Chain.Implementation/ChainManager.cs
@@ -0,0 +1,27 @@
+﻿using Pls.Chain.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pls.Chain.Implementation
+{
+    public class ChainManager
+    {
+        private List<IHandler> _handlers = new List<IHandler>();
+
+        public void Add(IHandler handler)
+        {
+            _handlers.Add(handler);
+        }
+
+        public void Handle(double dbvalue)
+        {
+            foreach(IHandler handler in _handlers)
+            {
+                handler.Handle(dbvalue);
+            }
+        }
+    }
+}
