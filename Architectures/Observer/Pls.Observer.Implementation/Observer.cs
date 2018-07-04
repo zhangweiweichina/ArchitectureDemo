fff941cbc92d67e1e64c0f2fd1c6e565f636f6e5
zhang weiwei <zhangweiweish@users.noreply.github.com>
2018-07-04 21:56:05 +0800

    Observer

---
+++ Architectures/Observer/Pls.Observer.Implementation/Observer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pls.Observer.Contract;
+
+namespace Pls.Observer.Implementation
+{
+    public class Observer : IObserver
+    {
+        public string Name { get; set; }
+
+        public Observer(string strName)
+        {
+            Name = strName;
+        }
+
+        public void Update(ISubject subject)
+        {
+            Console.WriteLine(subject.Description + " has notified " + Name);
+        }
+    }
+}
---
+++ Architectures/Observer/Pls.Observer.Implementation/Subject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pls.Observer.Contract;
+
+namespace Pls.Observer.Implementation
+{
+    public class Subject : ISubject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+        public string Description { get; private set; }
+
+        public Subject(string strName)
+        {
+            Description = strName;
+        }
+
+        public void Attach(IObserver ob)
+        {
+            _observers.Add(ob);
+        }
+
+        public void Detach(IObserver ob)
+        {
+            _observers.Remove(ob);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(this);
+            }
+        }
+    }
+}
