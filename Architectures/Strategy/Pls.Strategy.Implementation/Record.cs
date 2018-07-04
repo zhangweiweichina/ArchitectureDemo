6d7bb67cd431e3ce9328dec6115153a22b59cdf5
zhang weiwei <zhangweiweish@users.noreply.github.com>
2018-07-04 22:54:09 +0800

    Strategy

---
+++ Architectures/Strategy/Pls.Strategy.Implementation/Record.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pls.Strategy.Contract;
+
+namespace Pls.Strategy.Implementation
+{
+    public class Record
+    {
+        private double _x;
+        private double _y;
+        public Record(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public IAlgorithm Algorithm { get; set; }
+        public void Calc()
+        {
+            if(Algorithm != null)
+                Console.WriteLine("Result: " + Algorithm.Calc(_x, _y));
+        }
+    }
+}
